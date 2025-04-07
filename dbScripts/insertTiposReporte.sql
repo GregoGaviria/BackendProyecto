8705994a9a32dc1eb215704ec87a97ab28d2385c
grug <gregogaviria@gmail.com>
2025-04-07 13:28:52 -0600

    añadir caballo muerto en lista de reportes y eliminar script viejo

--- dbScripts/insertTiposReporte.sql
+++ dbScripts/insertTiposReporte.sql
@@ -4,3 +4,6 @@ INSERT INTO TipoReporte VALUES (3,"Deslizamiento");
 INSERT INTO TipoReporte VALUES (4,"Inundacion");
 INSERT INTO TipoReporte VALUES (5,"Infraestructura Peligrosa");
 INSERT INTO TipoReporte VALUES (6,"Otro");
+--en areas rurales de guanacaste es comun que la gente use caballos como transporte
+INSERT INTO TipoReporte Values (7,"Caballo Muerto Bloqueando Calle");
+INSERT INTO TipoReporte Values (8,"Vehiculo Bloqueando Calle")

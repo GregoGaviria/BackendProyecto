17a771a003b501108a8fdb5de803620b2f6defe7
grug <gregogaviria@gmail.com>
2025-04-07 19:50:14 -0600

    arreglos a el commit anterior

--- dbScripts/insertTiposReporte.sql
+++ dbScripts/insertTiposReporte.sql
@@ -6,4 +6,4 @@ INSERT INTO TipoReporte VALUES (5,"Infraestructura Peligrosa");
 INSERT INTO TipoReporte VALUES (6,"Otro");
 --en areas rurales de guanacaste es comun que la gente use caballos como transporte
 INSERT INTO TipoReporte Values (7,"Caballo Muerto Bloqueando Calle");
-INSERT INTO TipoReporte Values (8,"Vehiculo Bloqueando Calle")
+INSERT INTO TipoReporte Values (8,"Vehiculo Bloqueando Calle");
